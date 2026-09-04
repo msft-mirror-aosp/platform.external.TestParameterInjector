174328b76b2529ab2fb0cbf201690736bf381acb
Jens Nyman <jnyman@google.com>
2026-09-04 09:18:57 +0000

    Back out workarounds for the Android-23 reflection crash.

--- junit4/src/main/java/com/google/testing/junit/testparameterinjector/TestParameterMethodProcessor.java
+++ junit4/src/main/java/com/google/testing/junit/testparameterinjector/TestParameterMethodProcessor.java
@@ -54,8 +54,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import javax.annotation.Nullable;
 
 /**
@@ -213,8 +211,8 @@ class TestParameterMethodProcessor implements TestMethodProcessor {
       AnnotationWithMetadata annotationWithMetadata) {
     TestParameter annotation = annotationWithMetadata.annotation();
 
-    boolean valueIsSet = !valueIsEmpty(annotation);
-    Class<? extends TestParameterValuesProvider> valuesProvider = valuesProvider(annotation);
+    boolean valueIsSet = annotation.value().length > 0;
+    Class<? extends TestParameterValuesProvider> valuesProvider = annotation.valuesProvider();
     boolean valuesProviderIsSet = !valuesProvider.equals(DefaultTestParameterValuesProvider.class);
     checkState(
         !(valueIsSet && valuesProviderIsSet),
@@ -222,26 +220,8 @@ class TestParameterMethodProcessor implements TestMethodProcessor {
         annotation);
 
     if (valueIsSet) {
-      if (isAndroidMarshmallow()) {
-        System.err.println("Note from TestParameterInjector:");
-        System.err.println(
-            "Under Android 23, we have seen crashes on the operation we are about to perform.");
-        System.err.println(
-            "If you see a crash here, you may be able to work around it by changing your"
-                + " @TestParameter annotation to specify a valuesProvider instead of a value"
-                + " array.");
-        System.err.println("For background, see bug 287424109.");
-        System.err.println("We will now perform the operation...");
-      }
-      String[] value = annotation.value();
-      if (isAndroidMarshmallow()) {
-        System.err.println("The operation has succeeded.");
-        System.err.println(
-            "Any crash after now is unrelated to TestParameterInjector or is at least somewhat"
-                + " different from the usual crash.");
-      }
       return Optional.of(
-          FluentIterable.from(value)
+          FluentIterable.from(annotation.value())
               .transform(
                   v ->
                       TestParameterValue.maybeWrap(
@@ -253,74 +233,6 @@ class TestParameterMethodProcessor implements TestMethodProcessor {
               getValuesFromProvider(valuesProvider, annotationWithMetadata.context())));
     } else {
       return Optional.absent();
-    }
-  }
-
-  /**
-   * Returns the value of {@code annotation.valuesProvider()}, working around b/287424109.
-   *
-   * <p>Under Android API Level 23, we have seen calls to {@code valuesProvider()} sometimes crash
-   * with a segmentation fault. The crashes appear and disappear with "unrelated" changes elswhere
-   * in the binary. To work around them, we try to derive the value of {@code valuesProvider()} from
-   * the {@code toString()} value of the annotation, at least when running under an Android
-   * emulator. If we have any trouble, we fall back to calling {@code valuesProvider()}.
-   */
-  private static Class<? extends TestParameterValuesProvider> valuesProvider(
-      TestParameter annotation) {
-    if (!isAndroidMarshmallow()) {
-      return annotation.valuesProvider();
-    }
-
-    String string = annotation.toString();
-    // The format of toString() is unspecified.
-    // But under API Level 23, the only format we've seen so far is something like this:
-    // @com.google.testing.junit.testparameterinjector.TestParameter(value=[], valuesProvider=class
-    // com.google.testing.junit.testparameterinjector.TestParameterValuesProvider$DefaultTestParameterValuesProvider)
-    Matcher matcher = Pattern.compile("valuesProvider=class ([^),]*)").matcher(string);
-    if (!matcher.find()) {
-      // Maybe we're seeing a different format under a different version of Android?
-      // Rather than give up, we fall back to at least trying to read the value normally.
-      // Since the problem seems to be specific to API Level 23, we'd hope that this will succeed.
-      return annotation.valuesProvider();
-    }
-    try {
-      /*
-       * TODO: b/287424109 - Should we pass a specific ClassLoader, like use getContextClassLoader
-       * or the ClassLoader that loaded TestParameter or the class that contains the TestParameter
-       * usage (which we'd need to plumb through to here)?
-       */
-      return Class.forName(matcher.group(1)).asSubclass(TestParameterValuesProvider.class);
-    } catch (ClassNotFoundException e) {
-      // Falling back is unlikely to help here, but we might as well try.
-      // If we're lucky, it will somehow work, or at least the user will get a sensible failure.
-      // If we're unlucky, it will trigger the segfault.
-      return annotation.valuesProvider();
-    }
-  }
-
-  /**
-   * Checks whether {@code annotation.value()} is an empty array, working around b/287424109.
-   *
-   * <p>Compare {@link #valuesProvider}.
-   */
-  private static boolean valueIsEmpty(TestParameter annotation) {
-    return isAndroidMarshmallow()
-        ? annotation.toString().contains("TestParameter(value=[], valuesProvider=")
-        : annotation.value().length == 0;
-  }
-
-  private static boolean isAndroidMarshmallow() {
-    if (!System.getProperty("java.runtime.name", "").contains("Android")) {
-      return false;
-    }
-
-    try {
-      int version = (int) Class.forName("android.os.Build$VERSION").getField("SDK_INT").get(null);
-      int marshmallow =
-          (int) Class.forName("android.os.Build$VERSION_CODES").getField("M").get(null);
-      return version == marshmallow;
-    } catch (ReflectiveOperationException e) {
-      throw new LinkageError(e.getMessage(), e);
     }
   }
 
